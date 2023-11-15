8f65638f1890610101df333922daaafc08e19edc
Janwar Ksatria Pamungkas <janwarksatriapamungkas@hotmail.com>
2023-11-15 07:50:05 +0700

    Initial Commit

---
+++ app/src/main/java/com/hacktiv8/ecommerce3/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.hacktiv8.ecommerce3;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+
+    }
+
+}
