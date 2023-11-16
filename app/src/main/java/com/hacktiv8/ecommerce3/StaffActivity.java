c6495904d5fb79f50e76e51ed734d9172ce08920
Satria <ahmadsatriadimm93@gmail.com>
2023-11-16 18:33:14 +0800

    first commit

--- app/src/main/java/com/hacktiv8/ecommerce3/StaffActivity.java
+++ app/src/main/java/com/hacktiv8/ecommerce3/StaffActivity.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginUsersActivity extends AppCompatActivity {
+public class StaffActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_users);
+        setContentView(R.layout.activity_staff);
     }
 }
