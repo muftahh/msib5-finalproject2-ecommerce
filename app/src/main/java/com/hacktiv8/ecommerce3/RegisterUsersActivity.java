036651c5138082babe8056c12438c13eb1e381bc
Muftah <135422538+muftahh@users.noreply.github.com>
2023-11-17 08:50:21 +0700

    Revert "Muftah pull request"

--- app/src/main/java/com/hacktiv8/ecommerce3/RegisterUsersActivity.java
+++ app/src/main/java/com/hacktiv8/ecommerce3/RegisterUsersActivity.java
@@ -1,26 +1,14 @@
 package com.hacktiv8.ecommerce3;
 
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterUsersActivity extends AppCompatActivity {
-    private EditText inputIdMember, inputNamaMember, inputEmailMember, inputPasswordMember1, inputPasswordMember2;
-    private ImageButton bntSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_users);
-
-        inputIdMember = findViewById(R.id.inputIdMember);
-        inputNamaMember = findViewById(R.id.inputNamaMember);
-        inputEmailMember = findViewById(R.id.inputEmailMember);
-        inputPasswordMember1 = findViewById(R.id.inputPasswordMember1);
-        inputPasswordMember2 = findViewById(R.id.inputPasswordMember2);
-
-        bntSignUp = findViewById(R.id.bntSignUp);
     }
 }
