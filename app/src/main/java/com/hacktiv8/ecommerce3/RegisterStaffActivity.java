712a6aaba617d0556a991f0b9efffa1648bfc597
Janwar Ksatria Pamungkas <janwarksatriapamungkas@hotmail.com>
2023-11-18 10:06:17 +0700

    Add Registration Staff & Member Firebase Logic

--- app/src/main/java/com/hacktiv8/ecommerce3/RegisterStaffActivity.java
+++ app/src/main/java/com/hacktiv8/ecommerce3/RegisterStaffActivity.java
@@ -2,34 +2,109 @@ package com.hacktiv8.ecommerce3;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
+
 public class RegisterStaffActivity extends AppCompatActivity {
-    private EditText idStaff, namaLengkap, email, pw, konfirmasiPw;
+    private EditText mNamaLengkap, mEmail, mPassword, mKonfirmasiPassword;
+    private FirebaseAuth mAuth;
     ImageButton buttonSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_staff);
 
-        idStaff = findViewById(R.id.id_staff);
-        namaLengkap = findViewById(R.id.nama_lengkap);
-        email = findViewById(R.id.email);
-        pw = findViewById(R.id.pw);
-        konfirmasiPw = findViewById(R.id.konfirmasi_pw);
+        // ...
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        mNamaLengkap = findViewById(R.id.nama_lengkap);
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.pw);
+        mKonfirmasiPassword = findViewById(R.id.konfirmasi_pw);
         buttonSave = findViewById(R.id.save);
 
-        buttonSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), AdminActivity.class);
-                startActivity(intent);
-            }
-        });
+        buttonSave.setOnClickListener(v -> registerNewStaff());
 
     }
+
+
+    private void registerNewStaff() {
+        String namaLengkap, email, password,rePassword;
+        email = mEmail.getText().toString();
+        password = mPassword.getText().toString();
+        namaLengkap = mNamaLengkap.getText().toString();
+        rePassword = mKonfirmasiPassword.getText().toString();
+
+
+        if (TextUtils.isEmpty(namaLengkap)) {
+            Toast.makeText(getApplicationContext(), "Nama tidak boleh kosong!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Email tidak boleh kosong!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Password tidak boleh kosong", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (password.length() < 6) {
+            Toast.makeText(getApplicationContext(), "Isikan Password minimal 6 karakter", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (!password.equals(rePassword)) {
+            Toast.makeText(getApplicationContext(), "Password tidak sama, silahkan coba lagi", Toast.LENGTH_LONG).show();
+            mPassword.setText("");
+            mKonfirmasiPassword.setText("");
+            return;
+        }
+
+        // Pengecekan apakah email sudah terdaftar sebelumnya
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task1 -> {
+                    if (task1.isSuccessful()) {
+                        // Pendaftaran berhasil
+                        Toast.makeText(getApplicationContext(), "Akun Berhasil Didaftarkan!", Toast.LENGTH_LONG).show();
+
+                        // Pendaftaran berhasil, dapatkan ID pengguna
+                        String userId = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
+
+                        // Simpan data pengguna ke Realtime Database
+                        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance("https://hacktiv8-ecommerce3-default-rtdb.asia-southeast1.firebasedatabase.app/");
+                        DatabaseReference userRef = mDatabase.getReference("staffs").child(userId);
+
+                        userRef.child("name").setValue(namaLengkap); // Gantilah dengan data yang sesuai
+                        userRef.child("email").setValue(email);
+                        userRef.child("jobs").setValue("staff");
+
+                        // Tambahkan tindakan selanjutnya jika diperlukan
+                        Intent intent = new Intent(getApplicationContext(), AdminActivity.class);
+                        startActivity(intent);
+                    } else {
+                        // Pendaftaran gagal, cek apakah kesalahan disebabkan oleh email sudah terdaftar
+                        if (task1.getException() instanceof FirebaseAuthUserCollisionException) {
+                            // Email sudah terdaftar
+                            Toast.makeText(getApplicationContext(), "Email is already registered", Toast.LENGTH_LONG).show();
+                        } else {
+                            // Kesalahan lain, tampilkan pesan kegagalan
+                            Toast.makeText(getApplicationContext(), "Registration failed! Please try again later", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
+
 }
--- app/src/main/java/com/hacktiv8/ecommerce3/RegisterUsersActivity.java
+++ app/src/main/java/com/hacktiv8/ecommerce3/RegisterUsersActivity.java
@@ -1,26 +1,111 @@
 package com.hacktiv8.ecommerce3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
+
 public class RegisterUsersActivity extends AppCompatActivity {
-    private EditText inputIdMember, inputNamaMember, inputEmailMember, inputPasswordMember1, inputPasswordMember2;
-    private ImageButton bntSignUp;
+    private EditText inputNamaMember;
+    private EditText inputEmailMember;
+    private EditText inputPasswordMember1;
+    private EditText inputPasswordMember2;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_users);
 
-        inputIdMember = findViewById(R.id.inputIdMember);
+        // ...
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         inputNamaMember = findViewById(R.id.inputNamaMember);
         inputEmailMember = findViewById(R.id.inputEmailMember);
         inputPasswordMember1 = findViewById(R.id.inputPasswordMember1);
         inputPasswordMember2 = findViewById(R.id.inputPasswordMember2);
 
-        bntSignUp = findViewById(R.id.bntSignUp);
+        ImageButton bntSignUp = findViewById(R.id.bntSignUp);
+        bntSignUp.setOnClickListener(v -> registerNewUser());
     }
+
+    private void registerNewUser() {
+        String namaLengkap, email, password,rePassword;
+        email = inputEmailMember.getText().toString();
+        password = inputPasswordMember1.getText().toString();
+        namaLengkap = inputNamaMember.getText().toString();
+        rePassword = inputPasswordMember2.getText().toString();
+
+
+        if (TextUtils.isEmpty(namaLengkap)) {
+            Toast.makeText(getApplicationContext(), "Nama tidak boleh kosong!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Email tidak boleh kosong!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Password tidak boleh kosong", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (password.length() < 6) {
+            Toast.makeText(getApplicationContext(), "Isikan Password minimal 6 karakter", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (!password.equals(rePassword)) {
+            Toast.makeText(getApplicationContext(), "Password tidak sama, silahkan coba lagi", Toast.LENGTH_LONG).show();
+            inputPasswordMember1.setText("");
+            inputPasswordMember2.setText("");
+            return;
+        }
+
+        // Pengecekan apakah email sudah terdaftar sebelumnya
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task1 -> {
+                    if (task1.isSuccessful()) {
+                        // Pendaftaran berhasil
+                        Toast.makeText(getApplicationContext(), "Akun Berhasil Didaftarkan!", Toast.LENGTH_LONG).show();
+
+                        // Pendaftaran berhasil, dapatkan ID pengguna
+                        String userId = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
+
+                        // Simpan data pengguna ke Realtime Database
+                        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance("https://hacktiv8-ecommerce3-default-rtdb.asia-southeast1.firebasedatabase.app/");
+                        DatabaseReference userRef = mDatabase.getReference("members").child(userId);
+
+                        userRef.child("name").setValue(namaLengkap); // Gantilah dengan data yang sesuai
+                        userRef.child("email").setValue(email);
+                        userRef.child("jobs").setValue("user");
+
+                        // Tambahkan tindakan selanjutnya jika diperlukan
+                        Intent intent = new Intent(getApplicationContext(), AdminActivity.class);
+                        startActivity(intent);
+                    } else {
+                        // Pendaftaran gagal, cek apakah kesalahan disebabkan oleh email sudah terdaftar
+                        if (task1.getException() instanceof FirebaseAuthUserCollisionException) {
+                            // Email sudah terdaftar
+                            Toast.makeText(getApplicationContext(), "Email Telah terdaftar, silahkan lanjut login", Toast.LENGTH_LONG).show();
+                        } else {
+                            // Kesalahan lain, tampilkan pesan kegagalan
+                            Toast.makeText(getApplicationContext(), "Registrasi Gagal! Silahkan coba lagi nanti", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
+
 }
